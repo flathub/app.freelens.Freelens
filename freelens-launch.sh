f7b6c1898f24549f7fbf74617f0cc203f96ccf4e
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-03-19 00:40:59 +0100

    Extra args: --disable-gpu-compositing

--- freelens-launch.sh
+++ freelens-launch.sh
@@ -2,6 +2,9 @@
 
 EXTRA_ARGS+=(
   "--ozone-platform-hint=auto"
+  "--enable-features=WebRTCPipeWireCapturer"
+  "--enable-features=WaylandWindowDecorations"
+  "--disable-gpu-compositing"
 )
 
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
